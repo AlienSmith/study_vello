32331996c92d96e9ba0ddf5e1b75781fec999db0
MM-202202162345\Administrator <zhuanor@gmail.com>
2024-05-28 00:07:36 +0800

    update the version of vello_svg to latest

--- integrations/vello_svg/src/lib.rs
+++ integrations/vello_svg/src/lib.rs
@@ -1,11 +1,14 @@
-//! Append a [`usvg::Tree`] to a Vello [`SceneBuilder`]
+// Copyright 2023 the Vello Authors
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+//! Render a [`usvg::Tree`] to a Vello [`Scene`].
 //!
 //! This currently lacks support for a [number of important](crate#unsupported-features) SVG features.
 //! This is because this integration was developed for examples, which only need to support enough SVG
 //! to demonstrate Vello.
 //!
 //! However, this is also intended to be the preferred integration between Vello and [usvg], so [consider
-//! contributing](https://github.com/linebender/vello) if you need a feature which is missing.
+//! contributing](https://github.com/linebender/vello_svg) if you need a feature which is missing.
 //!
 //! [`render_tree_with`] is the primary entry point function, which supports choosing the behaviour
 //! when [unsupported features](crate#unsupported-features) are detected. In a future release where there are
@@ -19,7 +22,6 @@
 //! # Unsupported features
 //!
 //! Missing features include:
-//! - embedded images
 //! - text
 //! - group opacity
 //! - mix-blend-modes
@@ -27,230 +29,229 @@
 //! - masking
 //! - filter effects
 //! - group background
-//! - path visibility
-//! - path paint order
 //! - path shape-rendering
 //! - patterns
 
+pub mod util;
+
 use std::convert::Infallible;
-
-use usvg::NodeExt;
-use vello::kurbo::{Affine, BezPath, Rect};
-use vello::peniko::{Brush, Color, Fill, Stroke};
-
-pub use usvg;
+use vello::peniko::Fill;
 use vello::Scene;
 
-/// Append a [`usvg::Tree`] into a Vello [`SceneBuilder`], with default error handling
+/// Re-export vello.
+pub use vello;
+
+/// Re-export usvg.
+pub use usvg;
+
+/// Append a [`usvg::Tree`] into a Vello [`Scene`], with default error handling
 /// This will draw a red box over (some) unsupported elements
 ///
 /// Calls [`render_tree_with`] with an error handler implementing the above.
 ///
 /// See the [module level documentation](crate#unsupported-features) for a list of some unsupported svg features
-pub fn render_tree(sb: &mut Scene, svg: &usvg::Tree) {
-    render_tree_with(sb, svg, default_error_handler).unwrap_or_else(|e| match e {})
+pub fn render_tree(scene: &mut Scene, svg: &usvg::Tree) {
+    render_tree_with::<_, Infallible>(scene, svg, &mut util::default_error_handler)
+        .unwrap_or_else(|e| match e {});
 }
 
-/// Append a [`usvg::Tree`] into a Vello [`SceneBuilder`].
+/// Append a [`usvg::Tree`] into a Vello [`Scene`].
 ///
-/// Calls [`render_tree_with`] with [`default_error_handler`].
+/// Calls [`render_tree_with`] with [`util::default_error_handler`].
 /// This will draw a red box over unsupported element types.
 ///
 /// See the [module level documentation](crate#unsupported-features) for a list of some unsupported svg features
 pub fn render_tree_with<F: FnMut(&mut Scene, &usvg::Node) -> Result<(), E>, E>(
-    sb: &mut Scene,
+    scene: &mut Scene,
     svg: &usvg::Tree,
-    mut on_err: F,
+    error_handler: &mut F,
 ) -> Result<(), E> {
-    for elt in svg.root.descendants() {
-        let transform = {
-            let usvg::Transform { a, b, c, d, e, f } = elt.abs_transform();
-            Affine::new([a, b, c, d, e, f])
-        };
-        match &*elt.borrow() {
-            usvg::NodeKind::Group(_) => {}
-            usvg::NodeKind::Path(path) => {
-                let mut local_path = BezPath::new();
-                // The semantics of SVG paths don't line up with `BezPath`; we must manually track initial points
-                let mut just_closed = false;
-                let mut most_recent_initial = (0., 0.);
-                for elt in path.data.segments() {
-                    match elt {
-                        usvg::PathSegment::MoveTo { x, y } => {
-                            if std::mem::take(&mut just_closed) {
-                                local_path.move_to(most_recent_initial);
-                            }
-                            most_recent_initial = (x, y);
-                            local_path.move_to(most_recent_initial)
-                        }
-                        usvg::PathSegment::LineTo { x, y } => {
-                            if std::mem::take(&mut just_closed) {
-                                local_path.move_to(most_recent_initial);
-                            }
-                            local_path.line_to((x, y))
-                        }
-                        usvg::PathSegment::CurveTo {
-                            x1,
-                            y1,
-                            x2,
-                            y2,
-                            x,
-                            y,
-                        } => {
-                            if std::mem::take(&mut just_closed) {
-                                local_path.move_to(most_recent_initial);
-                            }
-                            local_path.curve_to((x1, y1), (x2, y2), (x, y))
-                        }
-                        usvg::PathSegment::ClosePath => {
-                            just_closed = true;
-                            local_path.close_path()
-                        }
-                    }
-                }
+    render_tree_impl(
+        scene,
+        svg,
+        &svg.view_box(),
+        &usvg::Transform::identity(),
+        error_handler,
+    )
+}
 
-                // FIXME: let path.paint_order determine the fill/stroke order.
+/// A helper function to render a tree with a given transform.
+fn render_tree_impl<F: FnMut(&mut Scene, &usvg::Node) -> Result<(), E>, E>(
+    scene: &mut Scene,
+    tree: &usvg::Tree,
+    view_box: &usvg::ViewBox,
+    ts: &usvg::Transform,
+    error_handler: &mut F,
+) -> Result<(), E> {
+    let ts = &ts.pre_concat(view_box.to_transform(tree.size()));
+    let transform = util::to_affine(ts);
+    scene.push_layer(
+        (1.0,1.0,1.0,1.0),
+        transform,
+        &vello::kurbo::Rect::new(
+            view_box.rect.left().into(),
+            view_box.rect.top().into(),
+            view_box.rect.right().into(),
+            view_box.rect.bottom().into(),
+        ),
+    );
+    render_group(
+        scene,
+        tree.root(),
+        &ts.pre_concat(tree.root().transform()),
+        error_handler,
+    )?;
+    scene.pop_layer();
 
-                if let Some(fill) = &path.fill {
-                    if let Some((brush, brush_transform)) =
-                        paint_to_brush(&fill.paint, fill.opacity)
-                    {
-                        // FIXME: Set the fill rule
-                        sb.fill(
-                            Fill::NonZero,
-                            transform,
-                            &brush,
-                            Some(brush_transform),
-                            &local_path,
-                        );
-                    } else {
-                        on_err(sb, &elt)?;
-                    }
-                }
-                if let Some(stroke) = &path.stroke {
-                    if let Some((brush, brush_transform)) =
-                        paint_to_brush(&stroke.paint, stroke.opacity)
-                    {
-                        // FIXME: handle stroke options such as linecap, linejoin, etc.
-                        sb.stroke(
-                            &Stroke::new(stroke.width.get() as f32),
-                            transform,
-                            &brush,
-                            Some(brush_transform),
-                            &local_path,
-                        );
-                    } else {
-                        on_err(sb, &elt)?;
-                    }
-                }
-            }
-            usvg::NodeKind::Image(_) => {
-                on_err(sb, &elt)?;
-            }
-            usvg::NodeKind::Text(_) => {
-                on_err(sb, &elt)?;
-            }
-        }
-    }
     Ok(())
 }
 
-/// Error handler function for [`render_tree_with`] which draws a transparent red box
-/// instead of unsupported SVG features
-pub fn default_error_handler(sb: &mut Scene, node: &usvg::Node) -> Result<(), Infallible> {
-    if let Some(bb) = node.calculate_bbox() {
-        let rect = Rect {
-            x0: bb.left(),
-            y0: bb.top(),
-            x1: bb.right(),
-            y1: bb.bottom(),
-        };
-        sb.fill(
-            Fill::NonZero,
-            Affine::IDENTITY,
-            Color::RED.with_alpha_factor(0.5),
-            None,
-            &rect,
-        );
+fn render_group<F: FnMut(&mut Scene, &usvg::Node) -> Result<(), E>, E>(
+    scene: &mut Scene,
+    group: &usvg::Group,
+    ts: &usvg::Transform,
+    error_handler: &mut F,
+) -> Result<(), E> {
+    for node in group.children() {
+        let transform = util::to_affine(ts);
+        match node {
+            usvg::Node::Group(g) => {
+                let mut pushed_clip = false;
+                if let Some(clip_path) = g.clip_path() {
+                    if let Some(usvg::Node::Path(clip_path)) = clip_path.root().children().first() {
+                        // support clip-path with a single path
+                        let local_path = util::to_bez_path(clip_path);
+                        scene.push_layer(
+                            (1.0,1.0,1.0,1.0),
+                            transform,
+                            &local_path,
+                        );
+                        pushed_clip = true;
+                    }
+                }
+
+                render_group(scene, g, &ts.pre_concat(g.transform()), error_handler)?;
+
+                if pushed_clip {
+                    scene.pop_layer();
+                }
+            }
+            usvg::Node::Path(path) => {
+                if path.visibility() != usvg::Visibility::Visible {
+                    continue;
+                }
+                let local_path = util::to_bez_path(path);
+
+                let do_fill = |scene: &mut Scene, error_handler: &mut F| {
+                    if let Some(fill) = &path.fill() {
+                        if let Some((brush, brush_transform)) =
+                            util::to_brush(fill.paint(), fill.opacity())
+                        {
+                            scene.fill(
+                                match fill.rule() {
+                                    usvg::FillRule::NonZero => Fill::NonZero,
+                                    usvg::FillRule::EvenOdd => Fill::EvenOdd,
+                                },
+                                transform,
+                                &brush,
+                                Some(brush_transform),
+                                &local_path,
+                            );
+                        } else {
+                            return error_handler(scene, node);
+                        }
+                    }
+                    Ok(())
+                };
+                let do_stroke = |scene: &mut Scene, error_handler: &mut F| {
+                    if let Some(stroke) = &path.stroke() {
+                        if let Some((brush, brush_transform)) =
+                            util::to_brush(stroke.paint(), stroke.opacity())
+                        {
+                            let conv_stroke = util::to_stroke(stroke);
+                            scene.stroke(
+                                &conv_stroke,
+                                transform,
+                                &brush,
+                                Some(brush_transform),
+                                &local_path,
+                            );
+                        } else {
+                            return error_handler(scene, node);
+                        }
+                    }
+                    Ok(())
+                };
+                match path.paint_order() {
+                    usvg::PaintOrder::FillAndStroke => {
+                        do_fill(scene, error_handler)?;
+                        do_stroke(scene, error_handler)?;
+                    }
+                    usvg::PaintOrder::StrokeAndFill => {
+                        do_stroke(scene, error_handler)?;
+                        do_fill(scene, error_handler)?;
+                    }
+                }
+            }
+            usvg::Node::Image(img) => {
+                if img.visibility() != usvg::Visibility::Visible {
+                    continue;
+                }
+                match img.kind() {
+                    usvg::ImageKind::JPEG(_)
+                    | usvg::ImageKind::PNG(_)
+                    | usvg::ImageKind::GIF(_) => {
+                        let Ok(decoded_image) = util::decode_raw_raster_image(img.kind()) else {
+                            error_handler(scene, node)?;
+                            continue;
+                        };
+                        let image = util::into_image(decoded_image);
+                        let Some(size) =
+                            usvg::Size::from_wh(image.width as f32, image.height as f32)
+                        else {
+                            error_handler(scene, node)?;
+                            continue;
+                        };
+                        let view_box = img.view_box();
+                        let new_size = view_box.rect.size();
+                        let (tx, ty) = usvg::utils::aligned_pos(
+                            view_box.aspect.align,
+                            view_box.rect.x(),
+                            view_box.rect.y(),
+                            view_box.rect.width() - new_size.width(),
+                            view_box.rect.height() - new_size.height(),
+                        );
+                        let (sx, sy) = (
+                            new_size.width() / size.width(),
+                            new_size.height() / size.height(),
+                        );
+                        let view_box_transform =
+                            usvg::Transform::from_row(sx, 0.0, 0.0, sy, tx, ty);
+                        scene.push_layer(
+                            (1.0,1.0,1.0,1.0),
+                            transform,
+                            &vello::kurbo::Rect::new(
+                                view_box.rect.left().into(),
+                                view_box.rect.top().into(),
+                                view_box.rect.right().into(),
+                                view_box.rect.bottom().into(),
+                            ),
+                        );
+                        let image_ts = util::to_affine(&ts.pre_concat(view_box_transform));
+                        scene.draw_image(&image, image_ts);
+
+                        scene.pop_layer();
+                    }
+                    usvg::ImageKind::SVG(svg) => {
+                        render_tree_impl(scene, svg, &img.view_box(), ts, error_handler)?;
+                    }
+                }
+            }
+            usvg::Node::Text(_) => {
+                error_handler(scene, node)?;
+            }
+        }
     }
+
     Ok(())
-}
-
-fn paint_to_brush(paint: &usvg::Paint, opacity: usvg::Opacity) -> Option<(Brush, Affine)> {
-    match paint {
-        usvg::Paint::Color(color) => Some((
-            Brush::Solid(Color::rgba8(
-                color.red,
-                color.green,
-                color.blue,
-                opacity.to_u8(),
-            )),
-            Affine::IDENTITY,
-        )),
-        usvg::Paint::LinearGradient(gr) => {
-            let stops: Vec<vello::peniko::ColorStop> = gr
-                .stops
-                .iter()
-                .map(|stop| {
-                    let mut cstop = vello::peniko::ColorStop::default();
-                    cstop.color.r = stop.color.red;
-                    cstop.color.g = stop.color.green;
-                    cstop.color.b = stop.color.blue;
-                    cstop.color.a = (stop.opacity * opacity).to_u8();
-                    cstop.offset = stop.offset.get() as f32;
-                    cstop
-                })
-                .collect();
-            let start: vello::kurbo::Point = (gr.x1, gr.y1).into();
-            let end: vello::kurbo::Point = (gr.x2, gr.y2).into();
-            let transform = Affine::new([
-                gr.transform.a,
-                gr.transform.b,
-                gr.transform.c,
-                gr.transform.d,
-                gr.transform.e,
-                gr.transform.f,
-            ]);
-            let gradient =
-                vello::peniko::Gradient::new_linear(start, end).with_stops(stops.as_slice());
-            Some((Brush::Gradient(gradient), transform))
-        }
-        usvg::Paint::RadialGradient(gr) => {
-            let stops: Vec<vello::peniko::ColorStop> = gr
-                .stops
-                .iter()
-                .map(|stop| {
-                    let mut cstop = vello::peniko::ColorStop::default();
-                    cstop.color.r = stop.color.red;
-                    cstop.color.g = stop.color.green;
-                    cstop.color.b = stop.color.blue;
-                    cstop.color.a = (stop.opacity * opacity).to_u8();
-                    cstop.offset = stop.offset.get() as f32;
-                    cstop
-                })
-                .collect();
-
-            let start_center: vello::kurbo::Point = (gr.fx, gr.fy).into();
-            let end_center: vello::kurbo::Point = (gr.cx, gr.cy).into();
-            let start_radius = 0_f32;
-            let end_radius = gr.r.get() as f32;
-            let transform = Affine::new([
-                gr.transform.a,
-                gr.transform.b,
-                gr.transform.c,
-                gr.transform.d,
-                gr.transform.e,
-                gr.transform.f,
-            ]);
-            let gradient = vello::peniko::Gradient::new_two_point_radial(
-                start_center,
-                start_radius,
-                end_center,
-                end_radius,
-            )
-            .with_stops(stops.as_slice());
-            Some((Brush::Gradient(gradient), transform))
-        }
-        usvg::Paint::Pattern(_) => None,
-    }
 }
