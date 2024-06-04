8aa548101ad2abdb8176a573afe899e288bd921c
MM-202202162345\Administrator <zhuanor@gmail.com>
2024-06-04 22:20:54 +0800

    Revert "compute pass contains labels"
    This reverts commit 06951804282e7552825dfb47f4451c9a381f0338.

--- src/wgpu_engine.rs
+++ src/wgpu_engine.rs
@@ -346,15 +346,11 @@ impl WgpuEngine {
                             &shader.bind_group_layout,
                             bindings,
                         )?;
-                        let mut cpass = encoder.begin_compute_pass(&wgpu::ComputePassDescriptor {
-                            label: Some(shader.label),
-                            ..Default::default()
-                        });
+                        let mut cpass = encoder.begin_compute_pass(&Default::default());
                         #[cfg(feature = "wgpu-profiler")]
                         let query = profiler
                             .begin_query(shader.label, &mut cpass, device)
-                            .with_parent(Some(&query)); 
-                        cpass.set_pipeline(&shader.pipeline);
+                            .with_parent(Some(&query)); cpass.set_pipeline(&shader.pipeline);
                         cpass.set_bind_group(0, &bind_group, &[]);
                         cpass.dispatch_workgroups(wg_size.0, wg_size.1, wg_size.2);
                         #[cfg(feature = "wgpu-profiler")]
@@ -394,10 +390,7 @@ impl WgpuEngine {
                             queue,
                             proxy,
                         );
-                        let mut cpass = encoder.begin_compute_pass(&wgpu::ComputePassDescriptor {
-                            label: Some(shader.label),
-                            ..Default::default()
-                        });
+                        let mut cpass = encoder.begin_compute_pass(&Default::default());
                         #[cfg(feature = "wgpu-profiler")]
                         let query = profiler
                             .begin_query(shader.label, &mut cpass, device)
